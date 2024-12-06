81efe081fc50fd6799f0ce30b81562d8e4a92679
ViniciusAlexsander <viniciusalexsander1@gmail.com>
2024-12-06 00:28:52 -0300

    Mensagem pra isadora

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home() {
   return <div>
     <h1>Oi isadora 😊</h1>
-    <p>eu sem você 🙁</p>
-    <p>eu com você 🥰</p>
+    <p>Só queria falar que gosto muito de você e vou fazer de tudo para que isso que estamos construindo de muito certo</p>
   </div>
 }
 
