792db9e40a898c040d80b44594c4628c02a6c593
ViniciusAlexsander <viniciusalexsander1@gmail.com>
2025-01-25 11:40:33 -0300

    feat: change emoji home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Oi isadora 😊</h1>
+      <h1>Oi isadora 🥰</h1>
       <p>
         Só queria falar que gosto muito de você e vou fazer de tudo para que
         isso que estamos construindo de muito certo
