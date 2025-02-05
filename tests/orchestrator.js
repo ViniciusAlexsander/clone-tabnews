aec7bb1c2e317ffcd79accae262c9e01bbeb0585
ViniciusAlexsander <viniciusalexsander1@gmail.com>
2025-02-05 00:26:05 -0300

    adds some improvements to the `orchestrator.js`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,11 +4,13 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    return retry(fetchStatusPage, { retries: 100 });
+    return retry(fetchStatusPage, { retries: 100, maxTimeout: 5000 });
 
-    async function fetchStatusPage() {
+    async function fetchStatusPage(bail, retryNumbers) {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
